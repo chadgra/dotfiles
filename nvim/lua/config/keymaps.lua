14c28be3f07c8de5aaf0aee6b70adbe36426f297
Chad Grant <chadgra@gmail.com>
2025-08-10 14:50:05 -0600

    Some easier keymaps for closing buffers

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader><delete>", function()
+  Snacks.bufdelete()
+end, { desc = "Close Buffer" })
+vim.keymap.set("n", "<leader>bo", Snacks.bufdelete.other, { desc = "Close Other Buffers" })
+vim.keymap.set("n", "<leader><CR>", "<cmd>%bd<cr>", { desc = "Close All Buffers" })
