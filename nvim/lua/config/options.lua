1a6896e3158b188a179a0cbae4519c9acaf1052c
Chad Grant <chad.grant@vivint.com>
2025-08-16 20:23:39 -0600

    Disable autoformat

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable auto-formatting of files on "Save"
+-- formatting can still be triggered manually with `leader>cf`
+vim.g.autoformat = false
