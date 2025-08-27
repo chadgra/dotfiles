3f3af28b559d9ce8f49c4012150bd16dfb08bb0e
Chad Grant <chad.grant@vivint.com>
2025-08-26 20:52:44 -0600

    Some "insert" mode keymappings
    Using both keymaps.lua for mapping `;;` and better-escape for mapping
    `jk` and `jj`

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Keymaps for working with buffers
 vim.keymap.set("n", "<leader><delete>", function()
   Snacks.bufdelete()
 end, { desc = "Close Buffer" })
 vim.keymap.set("n", "<leader>bo", Snacks.bufdelete.other, { desc = "Close Other Buffers" })
 vim.keymap.set("n", "<leader><CR>", "<cmd>%bd<cr>", { desc = "Close All Buffers" })
+
+-- Keymaps for insert mode
+  -- 'jk' is handled by better-escape.lua
+  -- vim.keymap.set("i", "jk", "<Esc>", { desc = "Normal mode" })
+  vim.keymap.set("i", ";;", "<C-o>", { desc = "Normal mode single operation" })
